e489e344bdc3af307588ad32dc03bc2c3ceb015d
Tsukamoto <tanabon2113@gmail.com>
2026-05-15 18:17:57 +0900

    会員登録画面仮完成

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,11 +9,53 @@
 </head>
 
 <body>
-    <header>
-        <a href="/">
-            <img src="storage/app/public/COACHTECHヘッダーロゴ.png" alt="COACHTECH">
-        </a>
-    </header>
+    <x-Header />
+    <main>
+        <div class="w-[680px] mt-[80px] mx-auto">
+
+            <h1 class="text-center font-bold text-[36px] mb-[40px]">会員登録</h1>
+
+            <form action="/register" method="post">
+                @csrf
+                <div class="mb-[30px]">
+                    <label for="name" class="text-[20px] font-bold">ユーザー名</label>
+                    <input type="text" id="name" name="name" class="border border-[#5F5F5F] rounded-md w-full h-[50px] px-3 text-[18px]">
+                    @error('name')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-[30px]">
+                    <label for="email" class="text-[20px] font-bold">メールアドレス</label>
+                    <input type="email" id="email" name="email" class="border border-[#5F5F5F] rounded-md w-full h-[50px] px-3 text-[18px]">
+                    @error('email')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-[30px]">
+                    <label for="password" class="text-[20px] font-bold">パスワード</label>
+                    <input type="password" id="password" name="password" class="border border-[#5F5F5F] rounded-md w-full h-[50px] px-3 text-[18px]">
+                    @error('password')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-[80px]">
+                    <label for="password_confirmation" class="text-[20px] font-bold">確認用パスワード</label>
+                    <input type="password" id="password_confirmation" name="password_confirmation" class="border border-[#5F5F5F] rounded-md w-full h-[50px] px-3 text-[18px]">
+                    @error('password_confirmation')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <button type="submit" class="w-full text-white bg-[#FF5555] py-[10px] rounded-md text-[26px]">登録する</button>
+                <div class="mt-[20px] text-center">
+                    <a href="/login" class="text-blue-500 hover:underline">ログインはこちら</a>
+                </div>
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
