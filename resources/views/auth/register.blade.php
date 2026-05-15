5aa03929cb863c0b69487f531979855fb9778bc2
Tsukamoto <tanabon2113@gmail.com>
2026-05-15 12:57:03 +0900

    ユーザー登録画面作成中

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>会員登録</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <header>
+        <a href="/">
+            <img src="storage/app/public/COACHTECHヘッダーロゴ.png" alt="COACHTECH">
+        </a>
+    </header>
+</body>
+
+</html>
