ea244c11a83ae3bdfd2134aa4a9eaa641859d5e1
Tsukamoto <tanabon2113@gmail.com>
2026-05-18 00:32:04 +0900

    WIP#2

--- resources/views/purchase/show.blade.php
+++ resources/views/purchase/show.blade.php
@@ -1,38 +1,47 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>商品詳細</title>
     @vite('resources/css/app.css')
 </head>
+
 <body>
     <x-header />
     <main class="w-full">
-        <div class="w-[1380px] grid md:grid-cols-2 mt-[40px] mb-[40px]">
-            <div class="w-[600px] aspect-square bg-black">
-                {{-- <img src="" alt=""> --}}
+        <div class="w-full grid md:grid-cols-2 my-[40px]">
+            <div class="w-[690px] aspect-square ml-auto p-[40px]">
+                <div class="bg-gray-300 rounded-xl w-full h-full"></div>
+                {{-- <img src="" alt="商品写真" class="w-full h-full object-cover> --}}
             </div>
 
-            <div class="w-full">
-                <h1 class="text-[30px] font-bold mb-[20px]">商品名がここに入る</h1>
-                <p class="text-[18px] text-[#5F5F5F] mb-[40px]">ブランド名</p>
-                <p class="text-[30px] font-bold">¥9,800</p>
-                <div class="flex">
-                    <div class="w-[40px]">
+            <div class="w-[690px] mr-auto px-[50px]">
+                <h1 class="text-[45px] font-bold mb-[10px]">商品名がここに入る</h1>
+                <p class="text-[18px] mb-[28px]">ブランド名</p>
+                <p class="text-[30px]">
+                    <span class="mr-[4px] text-[40px]">¥9,800</span>
+                    (税込み)
+                </p>
+                <div class="flex ml-[42px] mt-[28px]">
+                    <div class="w-[40px] text-center">
                         <img src="/images/ハートロゴ_デフォルト.png" alt="イイネ" class="w-full">
-                        <span>3</span>
+                        <span class="text-[18px]">3</span>
                     </div>
-                    <div class="ml-[40px] w-[38px]">
+                    <div class="ml-[62px] w-[38px] text-center">
                         <img src="/images/ふきだしロゴ.png" alt="コメント" class="w-full">
-                        <span>3</span>
+                        <span class="text-[18px]">3</span>
                     </div>
                 </div>
 
-                <a href="" class="w-full text-white bg-[#FF5555] py-[8px] rounded-md text-[24px] font-bold block text-center">購入手続きへ</a>
+                <a href=""
+                    class="w-full text-white bg-[#FF5555] py-[8px] rounded-md text-[24px] font-bold block text-center my-[22px]">
+                    購入手続きへ
+                </a>
 
                 <section>
-                    <h2>商品説明</h2>
+                    <h2 class="text-[36px] font-bold mb-[20px]">商品説明</h2>
                     <p>カラー：グレー</p>
                     <p>新品</p>
                     <p>商品の状態は良好です。傷もありません。</p>
@@ -40,23 +49,24 @@
                 </section>
 
                 <section>
-                    <h2>商品の情報</h2>
+                    <h2 class="text-[36px] font-bold mb-[20px]">商品の情報</h2>
                     <p>カテゴリー</p>
                     <p>商品の状態</p>
                 </section>
 
-                    <form action="">
-                        <h3>コメント</h3>
-                        <div class="flex">
-                            <div class="w-[70px] bg-[#D9D9D9]"></div>
-                            <p>admin</p>
-                        </div>
-                        <p>こちらにコメントが入ります。</p>
-
-                        <h3>商品へのコメント</h3>
+                <form action="">
+                    <h3 class="text-[24px] font-bold mb-[10px] text-[#5F5F5F]">コメント</h3>
+                    <div class="flex">
+                        <div class="w-[70px] bg-[#D9D9D9] rounded-full"></div>
+                        <p>admin</p>
                     </div>
+                    <p class="bg-[#E5E5E5]">こちらにコメントが入ります。</p>
+
+                    <x-form-input type="textarea" label="商品へのコメント" name="comment" class="h-[250px]"/>
+                </form>
             </div>
         </div>
     </main>
 </body>
+
 </html>
