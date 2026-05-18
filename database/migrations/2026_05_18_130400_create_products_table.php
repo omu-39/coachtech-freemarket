71849f0568626aca04b49ab071d7abc6cb003e57
Tsukamoto <tanabon2113@gmail.com>
2026-05-19 02:06:11 +0900

    WIP

---
+++ database/migrations/2026_05_18_130400_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('buyer_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->unsignedTinyInteger('category');
+            $table->string('name');
+            $table->string('description', 255);
+            $table->unsignedTinyInteger('status');
+            $table->string('brand')->nullable();
+            $table->integer('price');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
