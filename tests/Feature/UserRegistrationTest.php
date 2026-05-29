ce2b5324bbc7677b7f7d6cb9988724b4fd496c6f
Tsukamoto <tanabon2113@gmail.com>
2026-05-29 21:57:48 +0900

    会員登録機能テスト作成

--- tests/Feature/UserRegistrationTest.php
+++ tests/Feature/UserRegistrationTest.php
@@ -18,6 +18,9 @@ class UserRegistrationTest extends TestCase
     {
         $response = $this->post('/register', [
             'name' => '',
+            'email' => 'test@example.com',
+            'password' => '88888888',
+            'password_confirmation' => '88888888',
         ]);
 
         $response->assertSessionHasErrors([
@@ -28,7 +31,10 @@ class UserRegistrationTest extends TestCase
     public function test_user_register_validation_message_email(): void
     {
         $response = $this->post('/register', [
+            'name' => 'test',
             'email' => '',
+            'password' => '88888888',
+            'password_confirmation' => '88888888',
         ]);
 
         $response->assertSessionHasErrors([
@@ -39,7 +45,10 @@ class UserRegistrationTest extends TestCase
     public function test_user_register_validation_message_password(): void
     {
         $response = $this->post('/register', [
+            'name' => 'test',
+            'email' => 'test@example.com',
             'password' => '',
+            'password_confirmation' => '88888888',
         ]);
 
         $response->assertSessionHasErrors([
@@ -50,7 +59,10 @@ class UserRegistrationTest extends TestCase
     public function test_user_register_validation_message_password_min(): void
     {
         $response = $this->post('/register', [
+            'name' => 'test',
+            'email' => 'test@example.com',
             'password' => '7777777',
+            'password_confirmation' => '88888888',
         ]);
 
         $response->assertSessionHasErrors([
@@ -61,13 +73,27 @@ class UserRegistrationTest extends TestCase
     public function test_user_register_validation_message_password_confirmation(): void
     {
         $response = $this->post('/register', [
+            'name' => 'test',
+            'email' => 'test@example.com',
             'password' => '88888888',
-            'password_confirmation' => '8881234125'
+            'password_confirmation' => '8881234125',
         ]);
 
         $response->assertSessionHasErrors([
             'password' => 'パスワードと一致しません'
         ]);
+    }
+
+    public function test_user_can_register(): void
+    {
+        $response = $this->post('/register', [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => '88888888',
+            'password_confirmation' => '88888888',
+        ]);
+
+        $response->assertRedirect('/mypage/profile');
     }
 
 }
