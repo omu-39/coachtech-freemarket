b64b23b5256a396899d44a7bbd27ded784d83dfa
Tsukamoto <tanabon2113@gmail.com>
2026-06-02 02:44:11 +0900

    WIP

--- tests/Feature/ItemcontrollerTest.php
+++ tests/Feature/ItemcontrollerTest.php
@@ -210,6 +210,63 @@ class ItemControllerTest extends TestCase
 
     }
 
-    public function test_like_item_change_color(): void
-    {}
+    public function test_user_can_like_item_and_image_changes(): void
+    {
+        $user = User::factory()->create();
+        $item = Item::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('item.show', $item->id));
+
+        $response->assertStatus(200)
+            ->assertSee('/images/ハートロゴ_デフォルト.png');
+
+        $response = $this->actingAs($user)
+            ->post(route('like.store', $item->id));
+
+        $response->assertStatus(302)
+            ->assertRedirect(route('item.show', $item->id));
+
+        $this->assertDatabaseHas('likes', [
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get(route('item.show', $item->id));
+
+        $response->assertStatus(200)
+            ->assertSee('/images/ハートロゴ_ピンク.png');
+
+    }
+
+    public function test_user_can_unlike_item(): void
+    {
+        $user = User::factory()->create();
+        $item = Item::factory()->create();
+
+        $user->likes()->attach($item->id);
+
+        $this->assertDatabaseHas('likes', [
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+        ]);
+
+        $response = $this->actingAs($user)
+            ->delete(route('like.destroy', $item->id));
+
+        $response->assertStatus(302)
+            ->assertRedirect(route('item.show', $item->id));
+
+        $this->assertDatabaseMissing('likes', [
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get(route('item.show', $item->id));
+
+        $response->assertStatus(200)
+            ->assertSee('/images/ハートロゴ_デフォルト.png');
+    }
 }
